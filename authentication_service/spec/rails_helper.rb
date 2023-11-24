9873de865fc04411627b4475c61ec0847477dc55
Magesh <pingmagesh@gmail.com>
2023-11-24 10:33:49 +0530

    simplecov and factorybot config added

--- authentication_service/spec/rails_helper.rb
+++ authentication_service/spec/rails_helper.rb
@@ -5,6 +5,10 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require 'simplecov'
+SimpleCov.start
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -62,6 +66,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods
 
   def json_response
     JSON.parse(response.body)
